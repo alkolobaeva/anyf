099cad00193cbe3c5603682ac5b1fa5716727d5e
Aleksandra Kolobaeva <aleksandra.kolobaeva@finago.com>
2020-06-10 23:02:45 +0300

    small fix

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-echo 'Creating environment...'
-#conda create --name af python=3.7
-#conda activate af
+echo 'Create environment...'
+conda create --name af python=3.7
+conda activate af
 echo 'Installing requirements...'
-#pip install -r requirements.txt
+pip install -r requirements.txt
 cd code
 echo 'Training the model...'
 jupyter nbconvert --to python train.ipynb
 cd ..
 echo 'Starting API...'
-python code/api.gyp 
+python code/api.gyp
